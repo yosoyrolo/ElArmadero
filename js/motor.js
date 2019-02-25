13a842801675aea3365b4eaf87fac7501616380b
Esteban Devia <rolohd@gmail.com>
2019-02-25 11:01:42 -0500

    Desinstalar cosas que no necesitmaos

--- js/motor.js
+++ js/motor.js
@@ -1,5 +1,25 @@
 let motor = {};
 
-motor.save = function(url, nombre) {};
+motor.data = {
+  nombre: "Juego Nuevo"
+};
+motor.save = function(url, nombre) {
+  var fullUrl = url + "/" + nombre;
+  storage.set(fullUrl, this.data, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+motor.open = function(url) {
+  storage.get(url, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
+};
 
 console.log(storage);
